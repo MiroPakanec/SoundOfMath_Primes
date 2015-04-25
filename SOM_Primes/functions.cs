28a2f41e82c0172312f29ab03f063ddd1da66b2c
hoolby <a.hoolby@gmail.com>
2015-04-25 12:50:15 +0200

    First commit
    This is my first commit to the git containing the project files and
    aditional sound files is someone wants to change de notes

---
+++ SOM_Primes/functions.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using SOM_Primes.Properties;
+
+namespace SOM_PRIMES
+{
+    public class functions
+    {
+        //FIELDS
+        public static List<int> PrimeInts;
+        public static List<int> DigitalRootInts;
+        public static SoundPlayer Player1 = new SoundPlayer();
+        public static SoundPlayer Player2 = new SoundPlayer();
+        public static SoundPlayer Player3 = new SoundPlayer();
+        public static SoundPlayer Player4 = new SoundPlayer();
+        public static SoundPlayer Player5 = new SoundPlayer();
+        public static SoundPlayer Player6 = new SoundPlayer();
+        public static SoundPlayer Player7 = new SoundPlayer();
+        public static SoundPlayer Player8 = new SoundPlayer();
+        public static SoundPlayer Player9 = new SoundPlayer();
+        //METHODS
+        /// <summary>
+        ///     Initialize UI
+        /// </summary>
+        /// <returns></returns>
+        public static int UI()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("*** SoundOfMath_Primes ***\r\n");
+            Console.WriteLine("Choose # of prime numbers notes to generate in order to create the song:\r\n(0<#<1000)  ");
+            getvalue:
+            var input = Convert.ToInt32(Console.ReadLine());
+            if (input < 1 || input > 999)
+            {
+                Console.WriteLine("Wrong #, please insert a # between 0 and 1000");
+                goto getvalue;
+            }
+            Console.Clear();
+            Console.WriteLine("*** SoundOfMath_Primes ***\r\n");
+            Console.WriteLine("You chose to play " + input + " notes.");
+            return input;
+        }
+
+        /// <summary>
+        ///     Generate (#) of note values
+        /// </summary>
+        /// <param name="val"></param>
+        public static void gen_note_values(int val)
+        {
+            //Take the string from primes_f.txt file and separate them into list items
+            var pRawList = Resources.primes_f.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            //Generate list of prime ints based on the strings list
+            var pIntsList = new List<int>();
+            for (var i = 0; i < val; i++)
+                pIntsList.Add(Convert.ToInt32(pRawList[i]));
+
+            // System.IO.File.WriteAllText(@"C:\Users\Public\TestFolder\WriteLines.txt", "test");
+            //Assign values to the public list of primes
+            PrimeInts = pIntsList;
+
+            //Generate list of digital roots ints based on the prime ints list
+            var pDigitalRootList = new List<int>();
+            for (var j = 0; j < val; j++)
+                pDigitalRootList.Add(digital_root(pIntsList[j]));
+
+            //Assign values to the public list of digital roots
+            DigitalRootInts = pDigitalRootList;
+        }
+
+        /// <summary>
+        ///     Calculate the digital root of the given int
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static int digital_root(int number)
+        {
+            repeat:
+            var dr = 0;
+            while (number != 0)
+            {
+                dr += number%10;
+                number /= 10;
+            }
+            if (dr > 9)
+            {
+                number = dr;
+                goto repeat;
+            }
+
+            return dr;
+        }
+
+        /// <summary>
+        ///     Generate players
+        /// </summary>
+        public static void generate_players()
+        {
+            Player1.SoundLocation = "../../Resources/piano_chords/Amin7.wav";
+            Player2.SoundLocation = "../../Resources/piano_chords/major A middle.wav";
+            Player3.SoundLocation = "../../Resources/piano_chords/minor chords - Am higher.wav";
+            Player4.SoundLocation = "../../Resources/piano_chords/Cmin7 Low Pitch.wav";
+            Player5.SoundLocation = "../../Resources/piano_chords/major C middle.wav";
+            Player6.SoundLocation = "../../Resources/piano_chords/minor chords - Cm higher.wav";
+            Player7.SoundLocation = "../../Resources/piano_chords/Emin7.wav";
+            Player8.SoundLocation = "../../Resources/piano_chords/major E.wav";
+            Player9.SoundLocation = "../../Resources/piano_chords/minor chords - Em higher.wav";
+        }
+    }
+}
---
+++ SOM_Primes/main_program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+//
+namespace SOM_PRIMES
+{
+    internal class main_program
+    {
+        public static void Main()
+        {
+            // Initialize GUI and get # of notes to play
+            var nv = functions.UI();
+
+            // Generate # note values
+            functions.gen_note_values(nv);
+
+            // Generate players
+            functions.generate_players();
+
+            Console.WriteLine("Press any key to hear the song...\r\n");
+            Console.ReadKey();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var time = 600;
+            var l = 0;
+            List<int> COUNT = new List<int>();
+                for (int i =0;i<9;i++)
+                    COUNT.Add(0);
+            for (var j = 0; j < nv; j++)
+            {
+                Console.Write(j + ". Prime number = " + functions.PrimeInts[j] + " -> Digital root = " +
+                              functions.DigitalRootInts[j]);
+
+                if (time > 2000 || time < 800)
+                    time = 800;
+
+                if (functions.DigitalRootInts[j] > l)
+                {
+                    time = time + 200;
+                }
+                else
+                {
+                    time = time - 200;
+                }
+
+                l = functions.DigitalRootInts[j];
+                
+                switch (functions.DigitalRootInts[j])
+                {
+                    case 1:
+                        Console.Write("-> Note = {0} -> Delay = {1}\n\r", functions.DigitalRootInts[j], time);
+                        functions.Player1.Play();
+                        COUNT[0]++;
+                        break;
+
+                    case 2:
+                        Console.Write("-> Note = {0} -> Delay = {1}\n\r", functions.DigitalRootInts[j], time);
+                        functions.Player2.Play();
+                        COUNT[1]++;
+                        break;
+
+                    case 3:
+                        Console.Write("-> Note = {0} -> Delay = {1}\n\r", functions.DigitalRootInts[j], time);
+                        functions.Player3.Play();
+                        COUNT[2]++;
+                        break;
+
+                    case 4:
+                        Console.Write("-> Note = {0} -> Delay = {1}\n\r", functions.DigitalRootInts[j], time);
+                        functions.Player4.Play();
+                        COUNT[3]++;
+                        break;
+
+                    case 5:
+                        Console.Write("-> Note = {0} -> Delay = {1}\n\r", functions.DigitalRootInts[j], time);
+                        functions.Player5.Play();
+                        COUNT[4]++;
+                        break;
+
+                    case 6:
+                        Console.Write("-> Note = {0} -> Delay = {1}\n\r", functions.DigitalRootInts[j], time);
+                        functions.Player6.Play();
+                        COUNT[5]++;
+                        break;
+
+                    case 7:
+                        Console.Write("-> Note = {0} -> Delay = {1}\n\r", functions.DigitalRootInts[j], time);
+                        functions.Player7.Play();
+                        COUNT[6]++;
+                        break;
+
+                    case 8:
+                        Console.Write("-> Note = {0} -> Delay = {1}\n\r", functions.DigitalRootInts[j], time);
+                        functions.Player8.Play();
+                        COUNT[7]++;
+                        break;
+
+                    case 9:
+                        Console.Write("-> Note = {0} -> Delay = {1}\n\r", functions.DigitalRootInts[j], time);
+                        functions.Player9.Play();
+                        COUNT[8]++;
+                        break;
+                }
+
+                Thread.Sleep(time);
+            }
+            stopwatch.Stop();
+
+            //Display song duration
+            Console.WriteLine("\r\n The song was: {0} long.\r\n", stopwatch.Elapsed);
+
+            //Display note count
+            Console.WriteLine("Note count:");
+            for (int i=0;i<9;i++)
+                Console.WriteLine("     Note {0} - " +COUNT[i] + "times",i+1);
+            Console.ReadLine();
+        }
+    }
+}
